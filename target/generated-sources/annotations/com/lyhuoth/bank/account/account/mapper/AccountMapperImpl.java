0def0f0538ca8aeeeb5ab5bfd5e0a4f2263b1c93
lyhuoth73 <lyhuoth73@gmail.com>
2025-02-05 22:16:04 +0700

    tets

--- target/generated-sources/annotations/com/lyhuoth/bank/account/account/mapper/AccountMapperImpl.java
+++ target/generated-sources/annotations/com/lyhuoth/bank/account/account/mapper/AccountMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-02-05T22:01:05+0700",
+    date = "2025-02-05T22:11:41+0700",
     comments = "version: 1.6.2, compiler: Eclipse JDT (IDE) 3.41.0.z20250203-1602, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
